c2df127b989c494f50540cc46681d5d4cf2f2224
Francisco Renan Donaire Tablada <francisco_renan-dt@hotmail.com>
2019-08-09 22:06:56 -0600

    Cap 10 - Seccion 10.5

--- Cap10/Program.cs
+++ Cap10/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using Cap10.TimeClass;//    10.2
+using Cap10.TimeClass;//    10.2 - 10.4
+
 namespace Cap10
 {
     class Program
     {
         static void Main(string[] args)
         {
+            #region 10.5 Time Class Case Study: Overloaded Constructors
+            Time2Test.Principal();
+            #endregion
+
             #region 10.4 Referring to the Current Object's Members with the this Reference
-            ThisTest.Principal();
+            // ThisTest.Principal();
             #endregion
 
             #region 10.2 Time Class Case Study; Throwing Exceptions

eb532ac6c829a2aa99a3c1ca6a4f1d1c0228f93e
Francisco Renan Donaire Tablada <francisco_renan-dt@hotmail.com>
2019-08-22 10:47:10 -0600

    Cap13 - Seccion 13.3

--- Cap13/Program.cs
+++ Cap13/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using Cap13.ZeroNoException;    //
+using Cap13.ZeroNoException;    //13.2
+using Cap13.ZeroException;      //13.3
 namespace Cap13
 {
     class Program
     {
         static void Main(string[] args)
         {
+            #region 13.3 Example: Handling DivideByZeroExceptions and FormatExceptions
+            DivideByZeroExceptionHandling.Principal();
+            #endregion
+
             #region 13.2 Example: divide by Zero without Exception
-            DivideByZeroNoExceptionHandling.Principal();
+            // DivideByZeroNoExceptionHandling.Principal();
             #endregion
         }
     }
